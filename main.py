e88b59949befd20fde7917efbd16ce6ac6c6c672
AmandaIsrael <amanda_israel@hotmail.com>
2022-07-26 16:33:20 -0300

    especificação

---
+++ main.py
@@ -0,0 +1,12 @@
+# Os dois utilizam ordenação
+
+# Kruskal quadrático -> utilizar lista. 
+#   a   b   c   d - Grafos
+#   0   1   2   3 - índice
+
+# Próximo conjunto a ser computado terá de que ser verificado
+# para cada valor do conjunto 1:
+# a b c  <-  d
+#   1        2
+
+# Kruskal Union-Find -> utilizar árvore 
